5eb62dbb090d158a68ea8fbb0a72eb2470ea7074
leon fievet <fievet.leon@free.fr>
2025-01-01 20:08:39 +0100

    reset projet

--- src/constants/content/home.js
+++
@@ -1,7 +0,0 @@
-// src/constants/content/home.js
-export const homeContent = {
-    title: "Léon Fievet",
-    subtitle: "Étudiant en BUT Informatique à l'IUT de Montpellier",
-    description: "Passionné par le développement web et les nouvelles technologies",
-    contact: "fievet.leon@free.fr"
-};
