c0b839c1a1c23523f8e62bdcc0bf227c54a1a338
leon fievet <fievet.leon@free.fr>
2025-01-03 18:44:56 +0100

    up

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Change this to '/' since we're using username.github.io
   plugins: [react()],
   resolve: {
     alias: {
